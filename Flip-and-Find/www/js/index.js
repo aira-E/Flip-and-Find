b6271e036ed10a821c5ff7e4f2c387ad1b87aea3
Aira Estur <airaaltisestur@gmail.com>
2024-08-24 18:26:47 +0800

    accurate card spawning

--- Flip-and-Find/www/js/index.js
+++ Flip-and-Find/www/js/index.js
@@ -1,25 +1,5 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
-// Wait for the deviceready event before using any of Cordova's device APIs.
-// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
 document.addEventListener('deviceready', onDeviceReady, false);
+
 const gridContainer = document.querySelector(".grid-container");
 let cards = [];
 let firstCard, secondCard;
@@ -37,11 +17,11 @@ fetch("./json/cards.json")
     generateCards();
   });
 
-
 function shuffleCards() {
   let currentIndex = cards.length,
     randomIndex,
     temporaryValue;
+
   while (currentIndex !== 0) {
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex -= 1;
@@ -52,6 +32,8 @@ function shuffleCards() {
 }
 
 function generateCards() {
+  gridContainer.innerHTML = ""; // Clear previous cards if any
+
   for (let i = 0; i < 16; i++) { // Ensure only 16 cards are generated
     const card = cards[i];
     const cardElement = document.createElement("div");
@@ -113,9 +95,7 @@ function unflipCards() {
 }
 
 function resetBoard() {
-  firstCard = null;
-  secondCard = null;
-  lockBoard = false;
+  [firstCard, secondCard, lockBoard] = [null, null, false];
 }
 
 function restart() {
@@ -123,13 +103,10 @@ function restart() {
   shuffleCards();
   score = 0;
   document.querySelector(".score").textContent = score;
-  gridContainer.innerHTML = "";
   generateCards();
 }
 
 function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
-
-    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    document.getElementById('deviceready').classList.add('ready');
+  console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
+  document.getElementById('deviceready').classList.add('ready');
 }
