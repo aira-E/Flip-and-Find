3d6f20024e0eec4542e037ecea934a3fe9b59cc5
SiegfredLorelle <siegfredlorelle09@gmail.com>
2024-09-01 23:23:00 +0800

    Render top scores

--- Flip-and-Find/www/js/highscore.js
+++ Flip-and-Find/www/js/highscore.js
@@ -2,15 +2,19 @@
     document.addEventListener(event, onDeviceReady, false);
 });
 
+const highscoreTable = document.querySelector(".highscore-table > tbody");
+
 function onDeviceReady() {
-    const highscoreTable = document.querySelector(".highscore-table");
     const supabaseClient = initSupabase();
-    console.log(highscoreTable, supabaseClient);
-    highscoreTable.innerHTML = `<p>${supabaseClient ? "GUMANA" : "AYAW GUMANA"}</p>`;
+    // console.log(highscoreTable, supabaseClient);
+    // highscoreTable.innerHTML = `<p>${supabaseClient ? "GUMANA" : "AYAW GUMANA"}</p>`;
 
 
     (async () => {
         const topScores = await fetchTopScores(supabaseClient);
+        renderTopScores(topScores);
+        // highscoreTable.innerHTML = `<p>${topScores.score}</p>`;
+
     }) ();
 }
 
@@ -36,6 +40,38 @@ async function fetchTopScores(supabaseClient) {
         console.error(error);
     }
     else {
+        // highscoreTable.innerHTML = `<p>${data.score}</p>`;
         console.log("High Scores Data:", data, data.length);
+        return data;
     }
+}
+
+function renderTopScores(scores) {
+    console.log(scores);
+    scores.forEach((data, index) => {
+        const row = createTableRow(index + 1, data);
+        highscoreTable.appendChild(row);
+    });
+}
+
+function createTableRow(rank, data) {
+    const tr = document.createElement('tr');
+
+    const tdRank = document.createElement('td');
+    tdRank.textContent = rank;
+    tr.appendChild(tdRank);
+
+    const tdScore = document.createElement('td');
+    tdScore.textContent = data.score;
+    tr.appendChild(tdScore);
+
+    const tdDiff = document.createElement('td');
+    tdDiff.textContent = data.difficulty;
+    tr.appendChild(tdDiff);
+
+    const tdDate = document.createElement('td');
+    tdDate.textContent = data.achieved_at;
+    tr.appendChild(tdDate);
+    return tr;
+
 }
