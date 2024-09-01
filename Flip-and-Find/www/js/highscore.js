2c6ea4713558e999d48885988932789a2ac88b32
SiegfredLorelle <siegfredlorelle09@gmail.com>
2024-09-01 22:05:57 +0800

    Fetch top scores from database

--- Flip-and-Find/www/js/highscore.js
+++ Flip-and-Find/www/js/highscore.js
@@ -9,6 +9,9 @@ function onDeviceReady() {
     highscoreTable.innerHTML = `<p>${supabaseClient ? "GUMANA" : "AYAW GUMANA"}</p>`;
 
 
+    (async () => {
+        const topScores = await fetchTopScores(supabaseClient);
+    }) ();
 }
 
 function initSupabase() {
@@ -24,4 +27,15 @@ function initSupabase() {
     const supabaseKey = CONFIG.SUPABASE_API_KEY;
     const supabaseClient = supabase.createClient(supabaseUrl, supabaseKey);
     return supabaseClient;
+}
+
+async function fetchTopScores(supabaseClient) {
+    const { data, error } = await supabaseClient.rpc('get_top_scores');
+    if (error) {
+        console.error("ERROR: High scores not fetched.");
+        console.error(error);
+    }
+    else {
+        console.log("High Scores Data:", data, data.length);
+    }
 }
