e754e85a3d94e192b160979ee808bd5aa0dfe9d5
SiegfredLorelle <siegfredlorelle09@gmail.com>
2024-09-03 11:26:34 +0800

    Get the min highscore to tell which data to record

---
+++ Flip-and-Find/www/js/database/get-min-highscore.js
@@ -0,0 +1,46 @@
+["deviceready", "DOMContentLoaded"].forEach((event) => {
+  document.addEventListener(event, onDeviceReady, false);
+});
+
+const highScoreTable = document.querySelector(".highscore-table > tbody");
+
+function onDeviceReady() {
+  const supabaseClient = initSupabase();
+
+  (async () => {
+      const topScores = await fetchTopScores(supabaseClient);
+      renderMinTopScore(topScores);
+  }) ();
+}
+
+function initSupabase() {
+  if (typeof CONFIG === "undefined" || 
+      typeof CONFIG.SUPABASE_API_URL === "undefined" ||
+      typeof CONFIG.SUPABASE_API_KEY === "undefined" ) {
+      console.error(`ERROR: Supabase not initialized. \
+          Ensure config.js exists and it contains url and key.`)
+      return;
+  }
+
+  const supabaseUrl = CONFIG.SUPABASE_API_URL;
+  const supabaseKey = CONFIG.SUPABASE_API_KEY;
+  const supabaseClient = supabase.createClient(supabaseUrl, supabaseKey);
+  return supabaseClient;
+}
+
+async function fetchTopScores(supabaseClient) {
+  const { data, error } = await supabaseClient.rpc('get_top_scores');
+  if (error) {
+      console.error("ERROR: High scores not fetched.");
+      console.error(error);
+      return;
+  }
+  console.log("High Scores Data:", data, data.length);
+  return data;
+}
+
+function renderMinTopScore(scores) {
+  const minHighScore = document.querySelector(".min-high-score");
+  const minTopScore = scores.pop().score;
+  minHighScore.innerText = minTopScore;
+}
